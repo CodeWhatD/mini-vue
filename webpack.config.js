f12287280f5143317fe2b16208ba3bd5b1d2cdec
myduh <myduh@isoftstone.com>
2023-04-22 01:21:05 +0800

    remove note

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",// 让这个插件基于我们这个src中的index.html为模板，并且引入所有打包之后的资源
-      filename:'app.html', // 生成的.html文件叫什么名字
-      inject:'body' // 生成的script标签在什么标签里面
+      template: "./src/index.html",
+      filename:'app.html',
+      inject:'body'
     }),
   ],
   devServer: {
