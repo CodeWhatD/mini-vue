adafa33a6a924e3fce60ae9a7e8317d1fd5e3e83
myduh <myduh@isoftstone.com>
2023-04-24 21:32:54 +0800

    增加isReactive函数

--- src/reactive/index.js
+++ src/reactive/index.js
@@ -4,8 +4,14 @@ export const reactive = (target) => {
   if (!isObject(target)) {
     return target;
   }
+  if (isReactive(target)) {
+    return target;
+  }
   const proxy = new Proxy(target, {
     get(target, key, recevier) {
+      if (target.__isReactive) {
+        return true
+      }
       track(target, key);
       return Reflect.get(target, key, recevier);
     },
@@ -16,4 +22,12 @@ export const reactive = (target) => {
     },
   });
   return proxy;
+};
+
+/**
+ * 此函数可以判断该对象是否被代理
+ * 处理特殊情况：reactive(reactive({a:1}))
+ */
+export const isReactive = (target) => {
+  return !!(target && target.__isReactive);
 };
