ac6134bcae36374a0c14e112c90f5d182cc98306
myduh <myduh@isoftstone.com>
2023-05-28 21:27:35 +0800

    [ADD]增加mount元素基本逻辑& 增加元素prop增加的部分逻辑

--- src/runtime/render.js
+++ src/runtime/render.js
@@ -17,7 +17,48 @@ const mount = (vnode, container) => {
   }
 };
 
-const mountElement = (vnode, container) => {};
-const mountTextNode = (vnode, container) => {};
-const mountFragment = (vnode, container) => {};
+const mountElement = (vnode, container) => {
+  const { type, props } = vnode;
+  const el = document.createElement(type);
+  mountProps(props, el);
+  mountChildren(vnode, el);
+  container.appendChild(el);
+};
+const mountTextNode = (vnode, container) => {
+  const textNode = document.createTextNode(vnode);
+  container.appendChild(textNode);
+};
+const mountFragment = (vnode, container) => {
+  mountChildren(vnode, container);
+};
 const mountComponent = (vnode, container) => {};
+const mountProps = (props, el) => {
+  for (const key in props) {
+    const value = props[key];
+    switch (key) {
+      case "class":
+        el.className = value;
+      case "style":
+        for (const styleName in value) {
+          el.style[styleName] = value[styleName];
+        }
+      default:
+        // 处理onXX事件情况
+        if (/^on[^a-z]/.test(value)) {
+          const eventName = value.slice(2).toLowerCase(); // onClick => click
+          el.addEventListener(eventName, value);
+        }
+        break;
+    }
+  }
+};
+const mountChildren = (vnode, container) => {
+  const { shapeFlag, children } = vnode;
+  if (shapeFlag & ShapeFlgs.TEXT_CHILDREN) {
+    mountTextNode(vnode, container);
+  } else if (shapeFlag & ShapeFlgs.ARRAY_CHILDREN) {
+    children.forEach((child) => {
+      mount(child, container);
+    });
+  }
+};
