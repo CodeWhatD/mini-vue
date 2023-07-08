3ee7aff7f40446d9c9021d2fcb58339a51cf1540
myduh <myduh@isoftstone.com>
2023-07-08 12:07:14 +0800

    [FIX]解决分支切换导致的重复副作用执行

--- src/index.js
+++ src/index.js
@@ -3,21 +3,14 @@ import { effect } from "./reactive/effect";
 import { ref } from "./reactive/ref";
 import { computed } from "./reactive/computed";
 import { h, render, Fragment } from "./runtime";
-render(
-  h("ul", { style: { color: "red" } }, [
-    h("li", null, "first"),
-    h(Fragment, null, [h("li", null, "LAR")]),
-    h("li", null, "last"),
-  ]),
-  document.body
-);
-setTimeout(() => {
-  render(
-    h("ul", { style: { color: "red" } }, [
-      h("div", null, "first"),
-      h(Fragment, null, [h("div", null, "mid")]),
-      h("li", null, "last"),
-    ]),
-    document.body
-  );
-}, 1000);
+
+const obj = reactive({
+  ok: true,
+  name: "dmy",
+});
+
+window.obj = obj;
+
+effect(() => {
+  console.log("执行了函数"), obj.ok ? alert(obj.name) : 'false';
+});
--- src/reactive/effect.js
+++ src/reactive/effect.js
@@ -5,12 +5,14 @@ export const effect = (fn, options = {}) => {
     try {
       activeEffect = effectFn;
       activeEffectStack.push(activeEffect);
+      cleanup(effectFn);
       return fn();
     } finally {
       activeEffectStack.pop();
       activeEffect = activeEffectStack[activeEffectStack.length - 1];
     }
   };
+  effectFn.deps = [];
   if (!options.lazy) {
     effectFn();
   }
@@ -18,6 +20,14 @@ export const effect = (fn, options = {}) => {
     effectFn.scheduler = options.scheduler;
   }
   return effectFn;
+};
+
+const cleanup = (effectFn) => {
+  for (let i = 0; i < effectFn.deps.length; i++) {
+    effectFn.deps[i].delete(effectFn);
+  }
+  // 清空deps数组
+  effectFn.deps.length = 0;
 };
 
 const targetMap = new WeakMap(); // 创建一个响应式对象 依赖副作用的的一个结构，用来记录响应式对象中key所对应的副作用
@@ -32,9 +42,10 @@ export const track = (target, key) => {
   }
   let deps = depsMap.get(key); // 获取属性所对应的副作用 如果没有也重新set一个空的
   if (!deps) {
-    depsMap.set(key, (deps = new Map()));
+    depsMap.set(key, (deps = new Set()));
   }
-  deps.set(key, activeEffect);
+  deps.add(activeEffect);
+  activeEffect.deps.push(deps);
 };
 
 export const trigger = (target, key) => {
@@ -46,7 +57,13 @@ export const trigger = (target, key) => {
   if (!deps) {
     return;
   }
-  deps.forEach((effectFn) => {
+  const effectsToRun = new Set(); 
+  /**
+   * 这里新创建一个set原因为
+   * forEach在遍历set时，如果一个值已经被访问过，但该值被删除并重新添加到集合，如果此时forEach还没有结束，那么该值会被重新访问 解决这个问题很简单 我们可以在触发依赖的时候，构造一个set集合，并遍历
+   */
+  deps && deps.forEach((effectFn) => effectsToRun.add(effectFn));
+  effectsToRun.forEach((effectFn) => {
     // 执行调度
     if (effectFn.scheduler) {
       effectFn.scheduler(effectFn);
